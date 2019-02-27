cc708c786df660aa94b6f862d72d622b83fc3d4f
dcjones1 <dcejjones@gmail.com>
2019-02-27 10:10:38 -0500

    step 3 completed

--- src/index.js
+++ src/index.js
@@ -32,12 +32,13 @@ function showTrainer (trainer) {
     pokeList.appendChild(pokeName)
     pokeName.appendChild(relBtn)
 
-    // relBtn.addEventListener('click', releasePokemon)
+    relBtn.addEventListener('click', releasePokemon)
   })
 
   trainerDiv.className = 'card'
   trainerName.innerText = trainer.name
   addBtn.innerText = 'Add Pokemon'
+  pokeList.id = `trainer-${trainer.id}`
 
   main.appendChild(trainerDiv)
   trainerDiv.append(trainerName, addBtn, pokeList)
@@ -46,10 +47,12 @@ function showTrainer (trainer) {
 }
 
 // add a pokemon from the backend
-function addPokemon () {
+function addPokemon (e) {
+  debugger
+  let id = e.target.nextElementSibling.id.split('-')[1]
   fetch('http://localhost:3000/pokemons', {
     method: 'post',
-    body: JSON.stringify({trainer_id: 1}), // MAKE ME DYNAMIC
+    body: JSON.stringify({trainer_id: id}),
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json"
@@ -61,7 +64,7 @@ function addPokemon () {
 
 // add the new pokemon to the front end
 function showNewPokemon (pokemon) {
-  debugger
+  let pokeList = document.getElementById(`trainer-${pokemon.trainer_id}`)
   let pokeName = document.createElement('li')
   let relBtn = document.createElement('button')
 
@@ -72,5 +75,5 @@ function showNewPokemon (pokemon) {
   pokeList.appendChild(pokeName)
   pokeName.appendChild(relBtn)
 
-  // relBtn.addEventListener('click', releasePokemon)
+  relBtn.addEventListener('click', releasePokemon)
 }
